104452849e91a3034b9024570b363c765d50c3a2
Huzaifa Tariq <122553707+huzaifa898@users.noreply.github.com>
2025-01-08 16:17:38 +0500

    add footer to the veiw collections page

--- myapp/src/components/VeiwCollection.jsx
+++ myapp/src/components/VeiwCollection.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import CollectionHero from './CollectionHero'
 import TopCollections from './TopCollections'
+import Footer from './Footer'
 function VeiwCollection() {
   return (
     <div className='bg-gradient-to-b from-white to-pink-100'>
          <CollectionHero/> 
          <TopCollections/>
+         <Footer/>
     </div>
   )
 }
