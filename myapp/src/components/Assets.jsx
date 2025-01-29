c314f5fef7a977edadd84d94ff36e21a6d8161b9
Huzaifa Tariq <122553707+huzaifa898@users.noreply.github.com>
2025-01-29 12:50:35 +0500

    updated one other secction

--- myapp/src/components/Assets.jsx
+++ myapp/src/components/Assets.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Ass from '../Images/Ass.png'
+import Ass from '../Images/Ass.png';
 import Ass1 from '../Images/Ass1.png';
 import Ass2 from '../Images/Ass2.png';
 import Ass3 from '../Images/Ass3.png';
@@ -9,101 +9,39 @@ import Ass6 from '../Images/Ass6.png';
 
 function App() {
   return (
-    <div className=" w-full min-h-screen  flex justify-center items-center">
-      <div className="relative max-w-[1920px] w-full">
-        {/* Left Side Images */}
-        <div className="absolute left-0 top-0 hidden lg:block">
-          <img
-            src={Ass5}
-            alt="Left Image 1"
-            className=" w-[320px] h-[120px] top-[100px] left-[100px]"
-          />
-          <img
-            src={Ass6}
-            alt="Left Image 2"
-            className="absolute w-[170px] h-[170px] top-[300px] left-[60px]"
-          />
-          <img
-            src={Ass}
-            alt="Left Image 3"
-            className="absolute w-[120px] h-[120px] top-[500px] left-[60px]"
-          />
-          <img
-            src={Ass1}
-            alt="Left Image 4"
-            className="absolute w-[120px] h-[120px] top-[700px] left-[60px]"
-          />
+    <div className="w-full min-h-screen flex justify-center items-center">
+      <div className="relative w-full max-w-[1320px] h-[931px] flex justify-center items-center">
+        {/* Heading */}
+        <h1 className="absolute top-0 left-1/2 transform -translate-x-1/2 text-2xl font-apex text-center mt-0">
+          Turn your imagination into <br /> digital Asset
+        </h1>
+
+        {/* Left Column */}
+        <div className="absolute left-[240px] top-0 flex flex-col gap-16 hidden sm:flex">
+          <img src={Ass5} alt="Left Image 1" className="w-[110px] h-[110px]" style={{ marginLeft: '10px' }} />
+          <img src={Ass6} alt="Left Image 2" className="w-[170px] h-[170px]" style={{ marginLeft: '10px' }} />
+          <img src={Ass} alt="Left Image 3" className="w-[120px] h-[120px]" style={{ marginLeft: '0px' }} />
+          <img src={Ass1} alt="Left Image 4" className="w-[120px] h-[120px]" style={{ marginLeft: '100px' }} />
         </div>
 
-        {/* Right Side Images */}
-        <div className="absolute right-0 top-0 hidden lg:block">
-          <img
-            src={Ass2}
-            alt="Right Image 1"
-            className="absolute w-[170px] h-[170px] top-[100px] right-[60px]"
-          />
-          <img
-            src={Ass3}
-            alt="Right Image 2"
-            className="absolute w-[120px] h-[120px] top-[300px] right-[60px]"
-          />
-          <img
-            src={Ass4}
-            alt="Right Image 3"
-            className="absolute w-[170px] h-[170px] top-[500px] right-[60px]"
-          />
-          <img
-            src={Ass}
-            alt="Right Image 4"
-            className="absolute w-[170px] h-[170px] top-[700px] right-[60px]"
-          />
+        {/* Right Column */}
+        <div className="absolute top-0 flex flex-col gap-12 hidden sm:flex">
+          <img src={Ass2} alt="Right Image 1" className="w-[170px] h-[170px]" style={{ marginLeft: '725px' }} />
+          <img src={Ass3} alt="Right Image 2" className="w-[120px] h-[120px]" style={{ marginLeft: '850px' }} />
+          <img src={Ass4} alt="Right Image 3" className="w-[130px] h-[130px]" style={{ marginLeft: '850px' }} />
+          <img src={Ass} alt="Right Image 4" className="w-[140px] h-[140px]" style={{ marginLeft: '725px' }} />
         </div>
 
-        {/* Center Input Field */}
-        {/* <div
-          className="absolute flex flex-col items-center justify-center"
-          style={{
-            top: '400px',
-            left: '50%',
-            transform: 'translateX(-50%)',
-          }}
-        >
-          <input
-            type="text"
-            placeholder="Fantasy Creature holding a sword..."
-            className="w-[737px] px-6 py-3 border rounded-full shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
-          />
-          <button
-            className="mt-4 px-6 py-2 bg-red-500 text-white rounded-full shadow-lg hover:bg-red-600 transition duration-300"
-          >
-            Generate
-          </button>
-        </div>
-
-        {/* Mobile Layout */}
-        <div className=" flex flex-col items-center mt-8">
-          <div className="grid grid-cols-2 gap-4">
-            <img src={Ass5} alt="Left Image 1" className="w-20 h-20" />
-            <img src={Ass6} alt="Left Image 2" className="w-32 h-32" />
-            <img src={Ass} alt="Left Image 3" className="w-20 h-20" />
-            <img src={Ass1} alt="Left Image 4" className="w-20 h-20" />
-          </div>
-
-          <div className="grid grid-cols-2 gap-4 mt-6">
-            <img src={Ass2} alt="Right Image 1" className="w-32 h-32" />
-            <img src={Ass3} alt="Right Image 2" className="w-20 h-20" />
-            <img src={Ass4} alt="Right Image 3" className="w-32 h-32" />
-            <img src={Ass} alt="Right Image 4" className="w-32 h-32" />
-          </div>
-
-          <div className="mt-8 w-50 px-4">
+        {/* Center Input Field and Button */}
+        <div className="absolute flex flex-col items-center justify-center" style={{ top: '30%', left: '54%', transform: 'translate(-50%, -50%)' }}>
+          <div className="relative w-[300px] sm:w-[600px]">
             <input
-              type="text"
-              placeholder="Fantasy Creature holding a sword..."
-              className="w-full px-6 py-3 border rounded-full shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
+              type="email"
+              placeholder="Enter your email..."
+              className="w-full px-6 py-3 border rounded-full shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-black pr-20"
             />
             <button
-              className="mt-4 px-6 py-2 bg-red-500 text-white rounded-full shadow-lg hover:bg-red-600 transition duration-300 w-full"
+              className="absolute right-0 top-0 mt-1 mr-1 px-6 py-2 bg-red-500 text-white rounded-full shadow-lg hover:bg-red-600 transition duration-300"
             >
               Generate
             </button>
--- myapp/src/components/Home.jsx
+++ myapp/src/components/Home.jsx
@@ -22,8 +22,8 @@ function Home() {
           <Aucations/>
           <TopCollection/>
           <Sold/>
-           <Bost/>
-          
+           {/* <Bost/> */}
+            <Assets/>
           <Faq/>
           <Footer/>
     </div>
